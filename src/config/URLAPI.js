d0a55f836eabb6a5f9569ac28f3c0a32c8a28c38
Nguyen Dinh Thong <nguyenthong244466666@gmail.com>
2023-06-27 00:36:29 +0700

    'set URL vercel config "

--- src/config/URLAPI.js
+++ src/config/URLAPI.js
@@ -1,5 +1,5 @@
-const URLAPI = 'https://music-be-t40w.onrender.com';
-// const URLAPI = 'https://music-be-nine.vercel.app';
+// const URLAPI = 'https://music-be-t40w.onrender.com';
+const URLAPI = "https://music-be-nine.vercel.app";
 // const URLAPI = 'http://localhost:8000';
 
 export default URLAPI;
